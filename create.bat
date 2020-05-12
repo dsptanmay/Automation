9e621363ce3de256090591b0d21deff7d2943845
dsptanmay <dsp.tanmay@gmail.com>
2020-05-12 12:24:44 +0530

    added email/password option in .bat file added py code to use email/passwd fixed some naming in the .bat file

--- create.bat
+++ create.bat
@@ -1,20 +1,21 @@
 @ECHO off
 SET default_path=%CD%
+set /P email="Enter your email address: "
+set /P passwd="Enter your password: "
 SET /p gpath="Enter the path to which your projects folder is located: "
 CD /d "%gpath%"
 SET /p f1="Enter the name of the project folder: "
-mkdir %f1%
+MKDIR %f1%
 CD %f1%
 ECHO %CD%
 ECHO.>README.md
 git init
-ECHO Empty Repository Initialized
 ECHO Running Local.py to create repository
-python "%default_path%\local.py" %f1% > Output
+python "%default_path%\local.py" %f1% %email% %passwd% > Output
 ECHO Wait for the repository to be created and then press any key to continue!
 PAUSE
 SET /P MYVAR=<Output
-ECHO Repository created!
+ECHO Repository created !
 git remote add origin %MYVAR%
 ECHO Remote Added!
 git add README.md
@@ -22,9 +23,7 @@ git commit -m "initial commit"
 ECHO Initial Commit made
 git push -u origin master
 ECHO First commit pushed to master branch
-code .
-ECHO Code file opened
-ECHO Press any button to close 
-PAUSE
 DEL Output
-exit
+PAUSE
+ECHO Opening Code window
+code .
--- local.py
+++ local.py
@@ -3,26 +3,29 @@ from selenium.webdriver.common.keys import Keys
 import time
 import sys
 repName = sys.argv[1]
+emailAddress = sys.argv[2]
+passwd = sys.argv[3]
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://github.com/login")
 driver.implicitly_wait(3)
 
 emailButton = driver.find_element_by_xpath('//*[@id="login_field"]')
-emailButton.send_keys("dsp.tanmay@gmail.com")
+emailButton.send_keys(emailAddress)
 
 passwordButton = driver.find_element_by_xpath('//*[@id="password"]')
-passwordButton.send_keys("tanmay03bookworm#")
+passwordButton.send_keys(passwd)
 
 driver.find_element_by_xpath('//*[@id="login"]/form/div[4]/input[9]').click()
 time.sleep(4)
 driver.get('https://github.com/new')
 repo = driver.find_element_by_xpath('//*[@id="repository_name"]')
 repo.send_keys(repName)
-time.sleep(2)
+time.sleep(1)
 submitButton = driver.find_element_by_xpath(
     '//*[@id="new_repository"]/div[3]/button')
 submitButton.click()
 time.sleep(3)
 driver.close()
+print("\n")
 print("https://github.com/dsptanmay/{}.git".format(repName))
